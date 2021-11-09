5d09a1f2a18f6d54a4b2ff3e69872979f3a229e8
hdihd9162 <53191454+hdihd9162@users.noreply.github.com>
2021-11-09 21:45:18 +0330

    Create run.ino

---
+++ run.ino
@@ -0,0 +1,23 @@
+#include "DigiKeyboard.h"
+const int buttonPin = A3;
+int ledPin = 1;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(0, INPUT_PULLUP);
+}
+void loop() {
+  
+  
+  if(digitalRead(0)== HIGH){
+    digitalWrite(ledPin, HIGH);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);  
+    
+//    delay(500);
+  }
+  else{                     
+    digitalWrite(ledPin, LOW); 
+    
+  }                     
+}
+
